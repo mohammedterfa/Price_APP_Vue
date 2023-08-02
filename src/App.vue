659d8d915373f79a6c93a1e323444459df0b198a
mohammedterfa <mohammedterfa@gmail.com>
2023-08-02 09:59:49 +0300

    adding question functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,73 @@
 <script setup>
 import { ref, computed} from 'vue';
 
-const questions
+const questions = ref([
+  {
+    question: 'What is Vue ?',
+    answer: 0,
+    options: [
+      'A front end framework',
+      'A library',
+      'An ice cream maker'
+    ],
+    selected: null
+  },
+  {
+    question: 'What is Vuex ?',
+    answer: 2,
+    options: [
+      'Vue with an x',
+      'A cheese selection',
+      'State management library'
+    ],
+    selected: null
+  },
+
+  {
+    question: 'What is Vue Router used for ?',
+    answer: 1,
+    options: [
+      'Walkking in space',
+      'A routing library for Vue JS',
+      'Burger sauce',
+      'Quizzes'
+    ],
+    selected: null
+  },
+]);
+
+
+
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
+const score = computed(() => {
+  let value = 0;
+  questions.value.map(q => {
+    if(q.selected == q.answer){
+      value++
+    }
+  })
+  return value
+})
+
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value]
+  question.index = currentQuestion.value
+  return question
+})
+
+const SetAnswer = evt => {
+  questions.value[currentQuestion.value].selected = evt.target.value
+  evt.target.value = null
+}
+
+const NextQuestion = () => {
+  if(currentQuestion.value < questions.value.length - 1){
+    currentQuestion.value++
+  } else{
+    quizCompleted.value = true
+  }
+}
 </script>
 
 <template>
