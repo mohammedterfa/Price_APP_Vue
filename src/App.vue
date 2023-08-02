46214d7d5945e22896121a21e609438e44c52810
mohammedterfa <mohammedterfa@gmail.com>
2023-08-02 10:27:23 +0300

    implement the functionlaity to template

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,63 @@ const NextQuestion = () => {
 </script>
 
 <template>
-  <h1>Hello, World</h1>
+  <main class="app">
+    <h1>The Quiz</h1>
+
+    <section class="quiz" v-if="!quizCompleted">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrentQuestion.question }}</span>
+        <span class="score">Score {{ score }} / {{ questions.length }}</span>
+      </div>
+
+      <div class="options">
+        <label 
+          v-for="(option, index) in getCurrentQuestion.options" 
+          :key="index"
+          :class="`option ${
+            getCurrentQuestion.selected == index 
+              ? index == getCurrentQuestion.answer
+                ? 'corrent'
+                : 'wrong'
+              : ''
+          } ${
+            getCurrentQuestion.selected != null &&
+            index != getCurrentQuestion.selected
+              ? 'disabled'
+              : ''
+          }`"
+          >
+            <input 
+              type="radio" 
+              :name="getCurrentQuestion.index"
+              :value="index"
+              v-model="getCurrentQuestion.selected"
+              :disabled="getCurrentQuestion.selected"
+              @change="SetAnswer"
+              >
+            <span>{{ option }}</span>
+        </label>
+      </div>
+
+      <button
+        @click="NextQuestion"
+        :disabled="!getCurrentQuestion.selected"
+        >
+          {{ 
+            getCurrentQuestion.index == questions.length -1
+              ? 'Finish'
+              : getCurrentQuestion.selected == null
+                ? 'Select an option'
+                : 'Next Question'
+          }}
+      </button>
+    </section>
+
+    <section v-else>
+      <h2>You have finished the quiz!</h2>
+      <p>Your score  is {{ score }} / {{ questions.length }}</p>
+    </section>
+  </main>
 </template>
 
 
